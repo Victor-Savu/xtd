879c4b4c42c6b58b5ca9432a5acd03a2995f5622
Victor-Nicolae Savu <victor.nicolae.savu@gmail.com>
2015-09-11 19:25:04 +0200

    Added unit test for xtd::vector<T>::back()

--- test/vector.cc
+++ test/vector.cc
@@ -25,11 +25,31 @@ TEST(vector, pop) {
       []() { ADD_FAILURE() << "The vector should not be empty!"; });
 }
 
+TEST(vector, back) {
+  xtd::vector<int> v;
+  v.push(10).push(12);
+  v.back().match([](auto& v) { EXPECT_EQ(v, 12); },
+                 []() { ADD_FAILURE() << "The vector should not be empty!"; });
+  auto last = v.back().map([](auto& v) -> int { return v; });
+  auto popped = v.pop().map([](auto& v) -> int { return v; });
+  last.match(
+      [&popped](auto& v) {
+        popped.match([&v](auto& u) { int V{v}, U{u}; EXPECT_EQ(V, U); },
+                     []() {
+                       ADD_FAILURE()
+                           << "There should be something that was popped.";
+                     });
+      },
+      []() {
+        return ADD_FAILURE() << "There should be something at the back.";
+      });
+}
+
 TEST(vector, empty) {
   xtd::vector<int> v;
-  EXPECT_EQ(true, v.empty());
+  EXPECT_EQ(v.empty(), true);
   v.push(10);
-  EXPECT_EQ(false, v.empty());
+  EXPECT_EQ(v.empty(), false);
   v.pop();
-  EXPECT_EQ(true, v.empty());
+  EXPECT_EQ(v.empty(), true);
 }
